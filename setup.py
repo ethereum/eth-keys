1e0e3ff655de2f8b27fe869fe6f26bf576787823
Piper Merriam <pipermerriam@gmail.com>
2017-12-22 12:22:52 -0700

    Bump version: 0.1.0-beta.3 → 0.1.0-beta.4

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import (
 setup(
     name='eth-keys',
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-    version='0.1.0-beta.3',
+    version='0.1.0-beta.4',
     description="""Common API for Ethereum key operations.""",
     long_description_markdown_filename='README.md',
     author='Piper Merriam',
