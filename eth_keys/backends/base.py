0be52c6f2cc906dbda38065f1ddf464b656ef287
Jannik Luhn <jannik@brainbot.com>
2019-05-06 17:09:46 +0200

    Add compressed public key interface

--- eth_keys/backends/base.py
+++ eth_keys/backends/base.py
@@ -27,3 +27,11 @@ class BaseECCBackend(object):
     def private_key_to_public_key(self,
                                   private_key: PrivateKey) -> PublicKey:
         raise NotImplementedError()
+
+    def decompress_public_key_bytes(self,
+                                    compressed_public_key_bytes: bytes) -> bytes:
+        raise NotImplementedError()
+
+    def compress_public_key_bytes(self,
+                                  uncompressed_public_key_bytes: bytes) -> bytes:
+        raise NotImplementedError()
