c7614b624ef7b34450aef39d8fd7441da410c964
Jannik Luhn <jannik@brainbot.com>
2019-05-07 11:25:10 +0200

    Add test for compressed key validation

--- tests/core/test_key_and_signature_datastructures.py
+++ tests/core/test_key_and_signature_datastructures.py
@@ -14,6 +14,9 @@ from eth_utils import (
 
 from eth_keys import KeyAPI
 from eth_keys.backends import NativeECCBackend
+from eth_keys.exceptions import (
+    ValidationError,
+)
 
 
 MSG = b'message'
@@ -130,3 +133,14 @@ def test_compressed_bytes_conversion(key_api, private_key):
     compressed_bytes = public_key.to_compressed_bytes()
     assert len(compressed_bytes) == 33
     assert key_api.PublicKey.from_compressed_bytes(compressed_bytes) == public_key
+
+
+def test_compressed_bytes_validation(key_api, private_key):
+    valid_key = private_key.public_key.to_compressed_bytes()
+
+    with pytest.raises(ValidationError):
+        key_api.PublicKey.from_compressed_bytes(valid_key + b"\x00")
+    with pytest.raises(ValidationError):
+        key_api.PublicKey.from_compressed_bytes(valid_key[:-1])
+    with pytest.raises(ValidationError):
+        key_api.PublicKey.from_compressed_bytes(b"\x04" + valid_key[1:])
