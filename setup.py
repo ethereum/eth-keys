a95532d5b9fc0116c5fdae6dfa29ce2636dc44e7
Piper Merriam <pipermerriam@gmail.com>
2019-08-19 13:52:02 -0600

    Bump version: 0.3.0 → 0.3.1

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ deps['dev'] = (
 setup(
     name='eth-keys',
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-    version='0.3.0',
+    version='0.3.1',
     description="""Common API for Ethereum key operations.""",
     long_description_markdown_filename='README.md',
     author='Piper Merriam',
