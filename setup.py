1b52a4d6ad23f7d8819739a58426baf1c067d9e0
kclowes <kclowes@users.noreply.github.com>
2022-01-21 15:00:01 -0700

    Bump version: 0.3.3 → 0.3.4

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ with open('./README.md') as readme:
 setup(
     name='eth-keys',
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-    version='0.3.3',
+    version='0.3.4',
     description="""Common API for Ethereum key operations.""",
     long_description=long_description,
     long_description_content_type='text/markdown',
