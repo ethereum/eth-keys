2c101a767526a5e957ceef7cf5d9308b82d0c12a
Adam Jacobs <39714379+bllfr0g@users.noreply.github.com>
2025-02-04 04:14:15 -0800

    If we are passing in a non-native backend as a parameter to PrivateKey, then we can't call self.backend... until it's actually set, and THAT gets done by the superclass constructor.

--- eth_keys/datatypes.py
+++ eth_keys/datatypes.py
@@ -245,8 +245,9 @@ class PrivateKey(BaseKey, LazyBackend):
 
         self._raw_key = private_key_bytes
 
-        self.public_key = self.backend.private_key_to_public_key(self)
         super().__init__(backend=backend)
+
+        self.public_key = self.backend.private_key_to_public_key(self)
 
     def sign_msg(self, message: bytes) -> "Signature":
         message_hash = keccak(message)
