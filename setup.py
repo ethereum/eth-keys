8904f21f5953b2105baa5d3287cebd1465ad760c
pacrob <5199899+pacrob@users.noreply.github.com>
2025-04-07 11:40:09 -0600

    Bump version: 0.6.1 → 0.7.0

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ with open("./README.md") as readme:
 setup(
     name="eth-keys",
     # *IMPORTANT*: Don't manually change the version here. Use `make bump`, as described in readme
-    version="0.6.1",
+    version="0.7.0",
     description="""eth-keys: Common API for Ethereum key operations""",
     long_description=long_description,
     long_description_content_type="text/markdown",
